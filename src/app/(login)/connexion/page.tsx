7039e132a0737a1358ae21e61249012cd55f1b41
HyleF-YL <97347658+HyleF-YL@users.noreply.github.com>
2023-11-22 18:04:40 +0100

    question 2.2

--- src/app/(login)/connexion/page.tsx
+++ src/app/(login)/connexion/page.tsx
@@ -1,26 +1,32 @@
 "use client"
+import { z } from "zod"
 import { TextInput, PasswordInput } from '@mantine/core';
-import { useForm } from '@mantine/form';
+import { useForm, zodResolver } from '@mantine/form';
 import Link from 'next/link';
 import { Button } from 'tp-kit/components';
 import Layout from '../layout';
-import { useCallback } from 'react';
-import { ProductFiltersResult } from '../../../types';
 
 export default function Page() {
+
+    const schema = z.object({
+        email: z.string().email({message: "mail invalide"}),
+        password: z.string().min(6, {message: "Le mot de passe doit contenir au moins 6 caractères"})
+    })
 
     const form = useForm({
         initialValues: {
             email: "",
             password: ""
-        }
+        },
+        validate: zodResolver(schema)
     })
 
     return <Layout>
+        <h1>Connection</h1>
     <form onSubmit={form.onSubmit((values) => console.log(values))}>
         <TextInput label="Adresse email" withAsterisk placeholder='lin.guini@barilla.it' required {...form.getInputProps("email")}/>
         <PasswordInput label="Mot de passe" withAsterisk placeholder='Entrez votre mot de passe' required {...form.getInputProps("password")}/>
-        <Button type={"submit"}>S'inscrire</Button>
+        <Button fullWidth type={"submit"}>S'inscrire</Button>
         <Link href={"/inscription"}>Créer un compte</Link>
     </form> 
 </Layout>
--- src/app/(login)/inscription/page.tsx
+++ src/app/(login)/inscription/page.tsx
@@ -1,25 +1,52 @@
 "use client"
+import { z } from "zod"
 import { TextInput, PasswordInput } from '@mantine/core';
-import { useForm } from '@mantine/form';
+import { useForm, zodResolver } from '@mantine/form';
 import Link from 'next/link';
-import { Button } from 'tp-kit/components';
+import { Button, NoticeMessage, NoticeMessageData } from 'tp-kit/components';
 import Layout from '../layout';
+import { useState } from "react";
+
+const schema = z.object({
+    name: z.string().min(1, {message: "Name can't be empty"}),
+    email: z.string().email({message: "Invalid email"}),
+    password: z.string().min(6, {message: "password must be at least 6 characters"})
+})
+
+
 
 export default function Page() {
+
+    const handleErrors = (errors: typeof form.errors) => {
+       /* setSubmissionError(true)
+        setSubmissionSuccess(false)*/
+        setMessageToDisplay(<NoticeMessage type={"error"} onDismiss={() => setMessageToDisplay(null)} message="Une erreur s'est produite !"/>)
+    }
+
+    const handleSuccess = (values: typeof form.values) => {
+        console.log(values);
+        /*setSubmissionError(false)
+        setSubmissionSuccess(true)*/
+        setMessageToDisplay(<NoticeMessage type={"success"} onDismiss={() => setMessageToDisplay(null)} message="Votre inscription a bien été prise en compte. Validez votre adresse email pour vous connecter"/>)
+    }
+
+    const [submitionSuccess, setSubmissionSuccess] = useState(false)
+    const [submitionError, setSubmissionError] = useState(false)
+    const [messageToDisplay,setMessageToDisplay] = useState(null) as any
     const form = useForm({
         initialValues: {
             name: "",
             email: "",
             password: ""
         },
-        validate: {
-            password: (value) => (value.length < 6 ? 'password must be at least 6 characters' : null)
-        }
+        validate: zodResolver(schema),
     })
     return <Layout>
-        <form onSubmit={form.onSubmit((values) => console.log(values))}>
-            <TextInput label="Nom" withAsterisk placeholder='Entrez votre nom' {...form.getInputProps("name")}/>
-            <TextInput label="Adresse email" withAsterisk placeholder='lin.guini@barilla.it' {...form.getInputProps("email")}/>
+        {messageToDisplay}
+        <h1>Inscription</h1>
+        <form onSubmit={form.onSubmit(handleSuccess,handleErrors)}>
+            <TextInput label="Nom" withAsterisk description="Le nom qui sera utilisé pour vos commandes" placeholder='Entrez votre nom' required {...form.getInputProps("name")} />
+            <TextInput label="Adresse email" withAsterisk placeholder='lin.guini@barilla.it' required {...form.getInputProps("email")}/>
             <PasswordInput label="Mot de passe" withAsterisk placeholder='Entrez votre mot de passe' {...form.getInputProps("password")}/>
             <Button fullWidth type={"submit"}>S'inscrire</Button>
             <Link href={"/connexion"}>Déjà un compte, Se connecter</Link>
